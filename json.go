6d9b83388dd54e0c2b5e2d9bcc3a55a47da508b6
lcp <lcphutchinson@gmail.com>
2024-07-31 17:55:34 -0400

    addressing security issues for 5:3

--- json.go
+++ json.go
@@ -27,5 +27,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing response body: %v", err)
+		w.WriteHeader(500)
+		return
+	
 }
